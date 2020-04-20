245a7482413111113d6a3e7270a03776adf51cbb
Alberto <agonzalezbermejo13@gmail.com>
2020-04-20 13:10:43 +0200

    Correcion de error JUNIT clase 2

--- src/Clase2Test.java
+++ src/Clase2Test.java
@@ -9,15 +9,15 @@ class Clase2Test {
 		lista.add(5);
 		lista.add(5);
 		lista.add(5);
-		int resultado=Clase2.extracted(lista,5);
-		assertEquals(6,resultado);
+		int resultado=Clase2.extracted(lista,0);
+		assertEquals(5,resultado);
 	}
 	@Test
 	void testcuenta() {
 		lista.add(5);
 		lista.add(5);
-		int resultado=Clase2.contarNum(lista, 10);
-		assertEquals(resultado,20);
+		int resultado=Clase2.contarNum(lista, 0);
+		assertEquals(resultado,10);
 	}
 
 }
