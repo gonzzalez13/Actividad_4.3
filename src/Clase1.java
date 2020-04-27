daed8260e1edfa0b3c1a834cae286046ed9e7d13
Alberto <agonzalezbermejo13@gmail.com>
2020-04-27 17:31:39 +0200

    Clase1 y su JUNIT comentados

--- src/Clase1.java
+++ src/Clase1.java
@@ -1,6 +1,15 @@
 import java.util.Scanner;
+/**
+ * 
+ * @author Usuaio
+ *
+ */
 public class Clase1 {
 	static Scanner sc=new Scanner (System.in);
+	/**
+	 *Primero Creamos la arrays, despues hacemos que el usuario la rellene por pantalla 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 	int suma=0;
 	int numero [] = new int [5];
@@ -15,7 +24,14 @@ public class Clase1 {
 	System.out.println("hay "+numeros(suma,numero)+" que terminan en 3");
 	
 	}
-
+/**
+ * Tenemos la arrays rellenado con los 5 numeros
+ * ahora la recorremos con un for y con un if descublimos que numeros terminan en 3 
+ * los numero que terminan en 3 se hace un sumatorio para saber cuantos hay en la array. 
+ * @param suma --- Total de numeros que terminan en 3
+ * @param numero --- array donde se encuentran los numeros
+ * @return
+ */
 	public static int numeros(int suma,int numero []) {
 
 		for (int i = 0; i < numero.length; i++) {
--- src/Clase1Test.java
+++ src/Clase1Test.java
@@ -1,19 +1,22 @@
 import static org.junit.Assert.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.Test;
 
 public class Clase1Test {
-
+/**
+ * En el primer JUNIT es para saber si la operacion de saber cuantos numeros acabados en 3 esta correcta, en este caso solo ahi 1.
+ */
 	@Test
 	//Test con un 3 en el array
 	public void testNumeros() {
 		int numero [] = new int [] {1,2,3,4,5};
 		int resultado = Clase1.numeros(0,numero);
 		assertEquals(resultado, 1);
-		
+
 	}
-	
+	/**
+	 * En el SEGUNDO JUNIT es para saber si la operacion de saber cuantos numeros acabados en 3 esta correcta, en este caso solo ahi 2.
+	 */
 	@Test
 	//Test con dos 3 en el array
 	public void testNumeros1() {
@@ -22,7 +25,9 @@ public class Clase1Test {
 		assertEquals(resultado, 2);
 		
 	}
-	
+	/**
+	 * En el tercer JUNIT es para saber si la operacion de saber si da error por que tendria que dar 3 y solo hay en los parametros se le pone 2
+	 */
 	@Test
 	//Test fallido
 	public void testNumeros2() {
