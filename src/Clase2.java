a18a563a7dace2c7fe24f07199708d83ec694457
Manu <Manu@DESKTOP-FPL903D>
2020-04-28 12:11:43 +0200

    Clase2 y Clase2Test comentado

--- src/Clase2.java
+++ src/Clase2.java
@@ -1,11 +1,24 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+/**
+ * 
+ * @author Manu
+ *
+ */
 public class Clase2 {
 
 	static Scanner sc=new Scanner (System.in);
+	/**
+	 * Método main
+	 * En el método main llamo a las funciones que contienen la funcionalidad principal del programa.
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		/**
+		 * He creado un arraylist para contener los números que posteriormente se van a sumar.
+		 * También una variable boolean para crear un menú que el usuario manejará.
+		 * Por último, he creado una variable integer que será el sumatorio de los números contenidos en el arraylist.
+		 */
 		ArrayList<Integer> lista = new ArrayList<Integer>();
 		boolean  x = false;
 		int sumatorio=0;
@@ -16,6 +29,12 @@ public class Clase2 {
 		System.out.println(contarNum(lista,sumatorio));
 		System.out.println(extracted(lista, sumatorio));
 	}
+	/**
+	 * Esta función tiene el objetivo de calcular la media de la suma de los números contenidos en la variable sumatorio.
+	 * @param lista
+	 * @param sumatorio
+	 * @return
+	 */
 	public static int extracted(ArrayList<Integer> lista, int sumatorio) {
 		int tamañoLista;
 		int calculoMedia;
@@ -27,7 +46,12 @@ public class Clase2 {
 		calculoMedia=sumatorio/tamañoLista;
 		return calculoMedia;
 	}
-	
+	/**
+	 * Esta función la he creado para que se sumen los números que el usuario ha ido introduciendo en el arraylist.
+	 * @param lista
+	 * @param sumatorio
+	 * @return
+	 */
 	public static int contarNum(ArrayList<Integer> lista, int sumatorio) {
 		for (int i = 0; i < lista.size(); i++) {
 			sumatorio=sumatorio+lista.get(i);
@@ -36,7 +60,11 @@ public class Clase2 {
 		return sumatorio;
 	}
 	
-	
+	/**
+	 * Este método contiene el menú que el usuario manejará para indicar si quiere seguir introduciendo números en el arraylisto o no.
+	 * @param lista
+	 * @param x
+	 */
 	public static void introducirNumeros(ArrayList<Integer> lista, boolean x) {
 		String resp;
 		do {
--- src/Clase2Test.java
+++ src/Clase2Test.java
@@ -2,18 +2,28 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.ArrayList;
 import org.junit.jupiter.api.Test;
 
-class Clase2Test {
+public class Clase2Test {
 	ArrayList<Integer> lista = new ArrayList<Integer>();
+	/**
+	 * Test de la función que calcula la media
+	 * Este primer caso de prueba almacena 3 números en el arraylist, los números son el 5.
+	 * El resultado de la suma es 15 y del cálculo de la media 5. Se trata de un test sin errores.
+	 */
 	@Test
-	void testmedia() {
+	public void testmedia() {
 		lista.add(5);
 		lista.add(5);
 		lista.add(5);
 		int resultado=Clase2.extracted(lista,0);
 		assertEquals(5,resultado);
 	}
+	/**
+	 * Test de la función que calcula la suma
+	 * El segundo caso de prueba almacena 2 números en el arraylist, los números también son el 5.
+	 * El resultado de la suma es 10 por lo tanto también se trata de un test sin errores.
+	 */
 	@Test
-	void testcuenta() {
+	public void testcuenta() {
 		lista.add(5);
 		lista.add(5);
 		int resultado=Clase2.contarNum(lista, 0);
